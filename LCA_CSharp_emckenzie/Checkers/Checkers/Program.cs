ca98ed6fcab8eb952d68dc4d37b6ba01dfa073b1
emckenzie85 <e.mckenzie85@gmail.com>
2020-03-03 15:16:05 -0600

    ToDoApp

---
+++ LCA_CSharp_emckenzie/Checkers/Checkers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Checkers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+			Game newGame = new Game();
+
+            newGame.Start();
+            Console.ReadLine();
+        }
+    }
+
+    public enum Color { White, Black }
+
+    #region Position Struct
+    public struct Position
+    {
+        public int Row { get; private set; }
+        public int Column { get; private set; }
+        public Position(int row, int col)
+        {
+            Row = row;
+            Column = col;
+        }
+    }
+    #endregion
+}
